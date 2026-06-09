b52a02ac330f06055def5b3634511d91948ebe59
jorwi garcia <jgjg.0503@gmail.com>
2026-06-09 09:13:32 -0400

    familiar

--- models/User.js
+++ models/User.js
@@ -4,8 +4,9 @@ const userSchema = new Schema({
   nombre:       { type: String, required: true },
   username:     { type: String, required: true, unique: true },
   password:     { type: String, required: true },
-rol: { type: String, enum: ['administrador', 'especialista', 'admision', 'directivo', 'coordinador', 'baremo'], required: true },
+  rol: { type: String, enum: ['administrador', 'especialista', 'admision', 'directivo', 'coordinador', 'baremo'], required: true },
   esJefeServicio: { type: Boolean, default: false },
+  medicinaFamiliar: { type: Boolean, default: false },
   especialistaId: { type: Types.ObjectId, ref: 'Especialista', default: null },
   activo:       { type: Boolean, default: true },
 }, { timestamps: true });
